c5aba6331178ffd190a21e98ef3d92673b6ae016
aanchalsharmaa1024 <101443787+aanchalsharmaa1024@users.noreply.github.com>
2022-06-01 10:40:19 +0530

    delete

--- contibute.cpp
+++
@@ -1,9 +0,0 @@
-void multiply(ComplexNumbers const &c2)
-  {
-    int firsts=real*c2.real;
-    int outers=real*c2.imaginary;
-    int inners=imaginary*c2.real;
-    int lasts=(-1)*(imaginary*c2.imaginary);
-    real=firsts+lasts;
-    imaginary=outers+inners;
-  }
